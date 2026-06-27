92e8c30a4414ff82aab5aedbc2cf2e1de43cd364
Renen Isip <155710447+R3n3n@users.noreply.github.com>
2026-06-27 12:11:05 +0800

    Updated Code
    Signed-off-by: Renen Isip <155710447+R3n3n@users.noreply.github.com>

---
+++ BSIT_31E2_PRELIM_Q1_Isip_Renen/ConsoleApp1/ConsoleApp1/Airplane.cs
@@ -0,0 +1,13 @@
+namespace TransportChallenge;
+
+public class Airplane : Vehicle, IFlyable
+{
+    public override string Move()
+    {
+        return "Flying in the sky.";
+    }
+
+    public void Fly()
+    {
+    }
+}
---
+++ BSIT_31E2_PRELIM_Q1_Isip_Renen/ConsoleApp1/ConsoleApp1/Helicopter.cs
@@ -0,0 +1,17 @@
+﻿namespace TransportChallenge;
+
+public class Helicopter : Vehicle, IDriveable, IFlyable
+{
+    public override string Move()
+    {
+        return "Flying in the sky.";
+    }
+
+    public void Drive()
+    {
+    }
+
+    public void Fly()
+    {
+    }
+}
